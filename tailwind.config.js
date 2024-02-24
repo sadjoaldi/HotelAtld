2f5f012bf8a063411606ddb3a8a82bef23975e4c
Sadjoaldi <alsannealdi@outlook.com>
2024-02-24 17:31:24 +0100

    suite mise en forme des elements

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#13323A",
+      },
+      backgroundImage: {
+        "custom-image":
+          "url('https://img.freepik.com/photos-gratuite/interieur-chambre-chambre-hotel_23-2150683419.jpg') ",
+
+        "second-custom-image":
+          "url('https://img.freepik.com/photos-gratuite/jeune-femme-ayant-massage-du-visage-relaxant-dans-salon-spa_176420-7550.jpg')",
+      },
+      gridTemplateColumns: {
+        "custom-grid": "45% 55%",
+      },
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
