4f61b12a52a72d10347439d755eb6006dd164cf9
Sadjoaldi <alsannealdi@outlook.com>
2024-02-26 04:59:29 +0100

    creation fichier roomshtml et roomscss, les derniers details a régler

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,17 @@ module.exports = {
 
         "second-custom-image":
           "url('https://img.freepik.com/photos-gratuite/jeune-femme-ayant-massage-du-visage-relaxant-dans-salon-spa_176420-7550.jpg')",
+
+        "thirt-custom-image":
+          "url('https://images.unsplash.com/photo-1612152842505-f075576e6fee?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
+
       gridTemplateColumns: {
         "custom-grid": "45% 55%",
       },
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
       },
     },
   },
